fe2f8cc495feb5a0509447ad634b376d201c6444
Olivier Yiptong <oyiptong@chromium.org>
2021-04-06 00:35:07 -0700

    Compute-Pressure: Observer API and Null Backend
    This implements the initial Compute Pressure API with an observer that
    updates frequently with compute information.
    
    The backend infrastructure has been implemented with platform
    specific backends coming up in upcoming CLs.
    
    This change includes two security mitigations:
     * only observers belonging to the main frame get updated
      * only observers belonging to the presently focused window / tab get
         updated
    
    The latter security mitigation is tested manually.
    
    Fixed: 1183600
    Change-Id: I4fd929950b2a0d7810780e5af8d85063f4a3c6e3

---
+++ compute-pressure/compute_pressure.tentative.https.window.js
@@ -0,0 +1,33 @@
+// META: script=/resources/testdriver.js
+// META: script=/resources/testdriver-vendor.js
+// META: script=resources/utils.js
+
+'use strict';
+
+compute_pressure_test(async t => {
+  const update = await new Promise(async (resolve, reject) => {
+    try {
+      const observer = new ComputePressureObserver(update => {
+        resolve(update);
+      });
+      await observer.start();
+    } catch (e) {
+      reject(e);
+    }
+  });
+  assert_equals(typeof update.cpuUtilization, 'number');
+  assert_equals(typeof update.cpuSpeed, 'number');
+  assert_greater_than_equal(
+      update.cpuUtilization, 0, 'CPU Utilization is greater or equal to zero');
+  assert_greater_than_equal(
+      update.cpuSpeed, 0, 'CPU Speed is greater or equal to zero');
+}, 'ComputePressureObserver: Calls a callback with updates as data');
+
+compute_pressure_test(async t => {
+  const frame = document.body.appendChild(document.createElement('iframe'));
+  t.add_cleanup(() => frame.remove());
+
+  const obs = new frame.contentWindow.ComputePressureObserver(() => {});
+  await promise_rejects_dom(
+      t, 'SecurityError', frame.contentWindow.DOMException, obs.start());
+}, 'ComputePressureObserver: Does not start on sub-frame request');
